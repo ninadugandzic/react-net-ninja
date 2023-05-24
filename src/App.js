9761989afc3750309715c3467503fe024eecb599
Nina Dugandzic <ninadugandzic@gmail.com>
2023-05-24 14:32:55 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import Navbar from './Navbar';
+import Home from './Home';
+
+function App() {
+  const title = 'Welcome to the new blog';
+
+
+  return (
+    <div className="App">
+    <Navbar />
+    <div className="content">
+      <Home />
+      </div>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>The Dojo Blog</h1>
+            <a href="/">Home</a>
+            <a href="/create">New blog</a>
+        </nav>
+      );
+}
+ 
+export default Navbar;
